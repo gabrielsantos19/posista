565c74b44244e0e82f74a47c55cf1232dfc60727
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2023-05-16 12:35:54 -0300

    Adicionar imagens de uso livre

---
+++ assets/imagens_modelo/imagens_modelo.js
@@ -0,0 +1,27 @@
+const imagens_modelo = [
+    {
+        src: "assets/imagens_modelo/pose5.jpg",
+        nome_original: "man-1854195_1280.jpg",
+        credit: `Image by <a href="https://pixabay.com/users/pexels-2286921/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1854195">Pexels</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1854195">Pixabay</a>`,
+    },
+    {
+        src: "assets/imagens_modelo/pose4.jpg",
+        nome_original: 'portrait-3157821_1280.jpg',
+        credit: `Image by <a href="https://pixabay.com/users/artsysolomon-6715447/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3157821">Artsy Solomon</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3157821">Pixabay</a>`,
+    },
+    {
+        src: 'assets/imagens_modelo/pose6.jpg',
+        nome_original: 'action-1850677_1280.jpg',
+        credit: `Image by <a href="https://pixabay.com/users/pexels-2286921/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1850677">Pexels</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1850677">Pixabay</a>`,
+    },
+    {
+        src: 'assets/imagens_modelo/pose7.jpg',
+        nome_original: 'model-6318034_1280.jpg',
+        credit: `Image by <a href="https://pixabay.com/users/virat_maurya-9949610/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6318034">Virat Maurya</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6318034">Pixabay</a>`,
+    },
+    {
+        src: 'assets/imagens_modelo/pose8.jpg',
+        nome_original: 'people-2587069_1280.jpg',
+        credit: `Image by <a href="https://pixabay.com/users/stocksnap-894430/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2587069">StockSnap</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2587069">Pixabay</a>`,
+    },
+]
